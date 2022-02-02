7c30dd500985893cd36fb79ab6e4e244ab4aa62b
Irwin <irwinngo@gmail.com>
2022-02-01 20:54:09 -0500

    Completed Team Page version 1

--- src/components/pages/TeamPage/index.js
+++ src/components/pages/TeamPage/index.js
@@ -1,9 +1,41 @@
 import React from 'react';
+import './teamPage.css';
+import Container from './Container.js';
+import MemberCard from './MemberCard.js';
+import ExecutiveCard from './ExecutiveCard.js';
+import { ExecutiveCardInfo } from './ExecutiveCardInfo.js';
+import { MemberCardInfo } from './MemberCardInfo.js';
+import { FormerExecutiveCardInfo } from './FormerExecutiveCardInfo.js';
 
 const index = () => {
-  return <div>
-      This is the team page
-  </div>;
+  return <>
+      <div className="teamPageHeaderContainer">
+        <h1>
+          Meet The Project 
+        </h1>
+        <h1 style={{position: 'relative', marginTop: -60}}>
+          Holodeck Team
+        </h1>
+      </div>
+      <div className="teamPageCategoriesContainer">
+        <Container cards={<ExecutiveCard cardInfo={ExecutiveCardInfo}/>} title="Current Executive Members"/>
+
+        <svg className="teamPageLine">
+          <line x1="10%" y1="0" x2="90%" y2="0" className="teamPageLine"/>
+        </svg>
+
+        <Container cards={<MemberCard cardInfo={MemberCardInfo}/>} title="General Members"/>
+
+        <svg className="teamPageLine">
+          <line x1="10%" y1="0" x2="90%" y2="0" className="teamPageLine"/>
+        </svg>
+
+        <Container cards={<MemberCard cardInfo={FormerExecutiveCardInfo}/>} title="Former Executive Members"/>
+
+      </div>
+
+
+  </>;
 };
 
 export default index;
