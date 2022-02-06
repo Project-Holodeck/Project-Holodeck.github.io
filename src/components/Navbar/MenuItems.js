52519de4d9ff292f0aa96646d27107f9e4bdae71
Lia Weng <lia.xin.weng@outlook.com>
2022-02-06 14:21:47 -0500

    routes for different projects

--- src/components/Navbar/MenuItems.js
+++ src/components/Navbar/MenuItems.js
@@ -1,22 +1,22 @@
 export const MenuItems = [
-    {
-        title: 'HOME',
-        url: '/',
-        cName: 'nav-links'
-    },
-    {
-        title: 'TEAM',
-        url: '/team',
-        cName: 'nav-links'
-    },
-    {
-        title: 'PROJECTS',
-        url: '/projects',
-        cName: 'nav-links'
-    },
-    {
-        title: 'CONTACT',
-        url: '/',
-        cName: 'nav-links'
-    }
-]
+  {
+    title: 'HOME',
+    url: '/',
+    cName: 'nav-links',
+  },
+  {
+    title: 'TEAM',
+    url: '/team',
+    cName: 'nav-links',
+  },
+  {
+    title: 'PROJECTS',
+    url: '/projects/treadmill',
+    cName: 'nav-links',
+  },
+  {
+    title: 'CONTACT',
+    url: '/',
+    cName: 'nav-links',
+  },
+];
--- src/components/pages/ProjectPage/index.js
+++ src/components/pages/ProjectPage/index.js
@@ -1,47 +1,33 @@
 import React from 'react';
 import './index.css';
-import project_info from './project_info';
 
 import Banner from '../Banner/Banner';
 
-import arcade from './images/arcade.jpg';
-import leapmotion from './images/leapmotion.jpeg';
-import roomscanner from './images/roomscanner.jpg';
-import shoes from './images/shoes.jpg';
-import treadmill from './images/treadmill.jpg';
-
-const Project = ({ project, imgSrc }) => {
-  return (
-    <div className='projectContainer'>
-      <div className='projectImageContainer'>
-        <img className='projectImage' src={imgSrc} alt={project.name} />
-      </div>
-      <div className='projectDetailContainer'>
-        <div className='projectBorderContainerTop'>
-          <div className='projectBorder'></div>
-        </div>
-
-        <h3>About the {project.title.toLowerCase()} project</h3>
-        <p>{project.description}</p>
-        <p>Project Start Date: {project.start}</p>
-        <p>Completion Date: {project.end}</p>
-        <p>Project Members: {project.members}</p>
-
-        <div className='projectBorderContainerBottom'>
-          <div className='projectBorder'></div>
-        </div>
-      </div>
-    </div>
-  );
-};
-
-const index = () => {
-  const project = project_info[0];
-
+const index = ({ project, imgSrc }) => {
   return (
     <div>
       <Banner />
-      <Project project={project} imgSrc={roomscanner} />
+
+      <div className='projectContainer'>
+        <div className='projectImageContainer'>
+          <img className='projectImage' src={imgSrc} alt={project.name} />
+        </div>
+        <div className='projectDetailContainer'>
+          <div className='projectBorderContainerTop'>
+            <div className='projectBorder'></div>
+          </div>
+
+          <h3>About the {project.title.toLowerCase()} project</h3>
+          <p>{project.description}</p>
+          <p>Project Start Date: {project.start}</p>
+          <p>Completion Date: {project.end}</p>
+          <p>Project Members: {project.members}</p>
+
+          <div className='projectBorderContainerBottom'>
+            <div className='projectBorder'></div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
