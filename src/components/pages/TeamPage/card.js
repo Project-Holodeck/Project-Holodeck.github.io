cdcc1f7ac9cf9d755001ad0d05551a88f8763d12
Leonid Nediak <lnediak@gmail.com>
2022-02-12 22:51:06 -0500

    Roughly implemented teampage card design

---
+++ src/components/pages/TeamPage/card.js
@@ -0,0 +1,22 @@
+// card has attributes .className, .image, .name, .position,
+//   .discipline [is "discipline+year"], .link1, .link2
+const Card = ({card}) => {
+  return <>
+    <div className={"teamPageCard " + card.className}>
+      <img className="teamPageCardImg" src={card.image}/>
+      <br/>
+      <span className="teamPageCardName">{card.name}</span>
+      <br/>
+      <span className="teamPageCardDesc">{card.position}</span>
+      <br/>
+      <span className="teamPageCardDesc">{card.discipline}</span>
+      <br/>
+      <div className="teamPageCardLinks">
+        <a href={card.link1}><div className="teamPageCardLink1"/></a>
+        <a href={card.link2}><div className="teamPageCardLink2"/></a>
+      </div>
+    </div>
+  </>;
+}
+
+export default Card;
