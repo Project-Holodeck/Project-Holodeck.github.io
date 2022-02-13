691618b9166bdb904d782ab2a58ec0e2581457ab
Adam S <72041542+AdamS260@users.noreply.github.com>
2022-02-12 20:39:00 -0500

    Created general teampage layout

---
+++ src/components/pages/Banner/TeamPageBanner.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+// This banner will have a straight border instead of a wavy one
+const TeamPageBanner = () => {
+    return (
+        <div className='TeamPageBanner'>
+            <div className='TeamPageTitleWrapper'>
+                <h1>Our Team</h1>
+            </div>
+        </div>
+    );
+}
+
+export default TeamPageBanner;
--- src/components/pages/TeamPage/index.js
+++ src/components/pages/TeamPage/index.js
@@ -1,9 +1,30 @@
 import React from 'react';
+import TeamPageBanner from '../Banner/TeamPageBanner';
 
 const index = () => {
-  return <div>
-      This is the team page
-  </div>;
+  return (
+    <div>
+      <TeamPageBanner />
+      <div className='ExecCardContainer'>
+        <div className='CurrentExecutiveTitleWrapper'>
+
+        </div>
+
+      </div>
+      <div className='GeneralCardContainer'>
+        <div className='GeneralTitleWrapper'>
+
+        </div>
+
+      </div>
+      <div className='FormerCardContainer'>
+        <div className='FormerTitleWrapper'>
+
+        </div>
+        
+      </div>
+    </div>
+  );
 };
 
 export default index;
