386eed46a1d7c08a5cd745cf3006600769bc52fe
Adam S <72041542+AdamS260@users.noreply.github.com>
2022-02-13 23:21:32 -0500

    Modified CardGrid.js and added cards to main layout

--- src/components/pages/TeamPage/index.js
+++ src/components/pages/TeamPage/index.js
@@ -2,30 +2,57 @@ import React from 'react';
 import TeamPageBanner from '../Banner/TeamPageBanner';
 import CardGrid from './CardGrid';
 import Card from './card';
+import './index.css';
 
 let sampleImg = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAoAAAAKCAIAAAAC"
               + "UFjqAAAADUlEQVQY02NgGAWkAwABNgABGtWNFwAAAABJRU5ErkJggg==";
 
 let testingCard = 
-    <Card card={{
-        className: "teamPageCardStyle1",
-        image: sampleImg,
-        name: "Name 1",
-        position: "Position 1",
-        discipline: "Discipline+Year 1",
-        link1: "#",
-        link2: "#"
-    }}/>;
+  <Card card={{
+    className: "teamPageCardStyle1",
+    image: sampleImg,
+    name: "Name 1",
+    position: "Position 1",
+    discipline: "Discipline+Year 1",
+    link1: "#",
+    link2: "#"
+  }}/>;
 
-let myCards = [testingCard, testingCard, testingCard];
+let myCards = [testingCard, testingCard, testingCard, testingCard];
+
+let testingCard2 = 
+  <Card card={{
+    className: "teamPageCardStyle2",
+    image: sampleImg,
+    name: "Name 2",
+    position: "Position 2",
+    discipline: "Discipline+Year 2",
+    link1: "#",
+    link2: "#"
+  }}/>
+
+let myCards2 = [testingCard2, testingCard2, testingCard2, testingCard2, testingCard2];
+
+let testingCard3 = 
+  <Card card={{
+    className: "teamPageCardStyle1",
+    image: sampleImg,
+    name: "Name 1",
+    position: "Position 1",
+    discipline: "Discipline+Year 1",
+    link1: "#",
+    link2: "#"
+  }}/>;
+
+let myCards3 = [testingCard3, testingCard3, testingCard3, testingCard3, ]
 
 const index = () => {
   return (
-    <div>
+    <div >
       <TeamPageBanner />
       <div className='ExecCardContainer'>
         <div className='CurrentExecutiveTitleWrapper'>
-
+          <h1>Current Executive Members</h1>
         </div>
         <div className='CardGridContainer'>
           <CardGrid cards={myCards}/>
@@ -33,18 +60,18 @@ const index = () => {
       </div>
       <div className='GeneralCardContainer'>
         <div className='GeneralTitleWrapper'>
-
+          <h1>General Members</h1>
         </div>
         <div className='CardGridContainer'>
-          
+          <CardGrid cards={myCards2} />
         </div>
       </div>
       <div className='FormerCardContainer'>
         <div className='FormerTitleWrapper'>
-
+          <h1>Former Executive Members</h1>
         </div>
         <div className='CardGridContainer'>
-          
+          <CardGrid cards={myCards3} />
         </div>
       </div>
     </div>
