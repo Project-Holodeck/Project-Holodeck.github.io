3d8678d36ebde71a630aed0334d70f4c65509ea1
ozzy <o.sultan2811@gmail.com>
2022-02-15 00:44:36 -0500

    banner fixes

---
+++ src/components/pages/Banner/BannerInfo.js
@@ -0,0 +1,12 @@
+const info = [
+  {
+    title: "Projects",
+    blurb: "Find out more about the projects we have done!",
+  },
+  {
+    title: "Team",
+    blurb: "Wondering who makes up our design team?",
+  },
+];
+
+export default info;
