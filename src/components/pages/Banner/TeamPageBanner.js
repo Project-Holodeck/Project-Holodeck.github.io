6bb81ed008e6313671c9570b713ecd53d9e39c7a
Adam S <72041542+AdamS260@users.noreply.github.com>
2022-02-14 22:44:43 -0500

    Added paragraph underneath teampage title.

--- src/components/pages/Banner/TeamPageBanner.js
+++ src/components/pages/Banner/TeamPageBanner.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import './TeamPageBanner.css';
+import './ProjectPageBanner.css';
 
 // This banner will have a straight border instead of a wavy one
 const TeamPageBanner = () => {
     return (
         <div className='TeamPageBanner'>
-            <div className='TeamPageTitleWrapper'>
-                <p className='ourTeamHeader'><arrow>⌜</arrow>Our<x> Team</x><arrow>⌟</arrow></p>
+            <div className='box0'>
+                <div className='box1'>
+                    <p className='ourTeamHeader'><arrow>⌜</arrow>Our<x> Team</x><arrow>⌟</arrow></p>
+                </div>
+            </div>
+            <div className='box2'>
+                <div className='projectBorderContainerTop'>
+                    <div className='projectBorder'></div>
+                </div>
+                <div className='box4'>
+                    <p>Wondering who makes up our design team? <br />
+                    Read here to learn about our...</p>
+                </div>
+                <div className='projectBorderContainerBottom'>
+                    <div className='projectBorder'></div>
+                </div>
             </div>
         </div>
     );
