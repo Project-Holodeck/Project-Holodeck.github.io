3ad5507a9b0fe25a482c5b619f571372cb52812b
Adam S <72041542+AdamS260@users.noreply.github.com>
2022-02-19 12:10:01 -0500

    Added CurrentExecInfo.js

---
+++ src/components/pages/TeamPage/teamInfo.js
@@ -0,0 +1,34 @@
+const CurrentExecInfo = [
+    {
+        name: 'Andrew Ilersich',
+        image: './images/Andrew Ilersich.png',
+        position: 'Position 1',
+        discipline: 'Discipline+Year 1'
+    },
+    {
+        name: 'Ben Goodchild',
+        image: './images/Ben Goodchild.JPG',
+        position: 'Position 1',
+        discipline: 'Discipline+Year 1'
+    },
+    {
+        name: 'Ghamr Saeed',
+        image: './images/Ghamr Saeed.jpg',
+        position: 'Position 1',
+        discipline: 'Discipline+Year 1'
+    },
+    {
+        name: 'Irwin Ngo',
+        image: './images/Irwin Ngo.png',
+        position: 'Position 1',
+        discipline: 'Discipline+Year 1'
+    },
+    {
+        name: 'Parth Dua',
+        image: './images/Parth Dua.jpg',
+        position: 'Position 1',
+        discipline: 'Discipline+Year 1'
+    }
+]
+
+export { CurrentExecInfo };
