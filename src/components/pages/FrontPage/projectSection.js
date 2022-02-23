dd21fe03dd512b8ccdae0abe4a7841c771f16d4c
Cole Kim <mrcolekim@gmail.com>
2022-02-23 13:57:27 -0500

    FrontPage Updates, plus some style tweaks, namely to z-indexing. Making a commit before working extensively on slideshow and Midsec on frontpage.

--- src/components/pages/FrontPage/projectSection.js
+++ src/components/pages/FrontPage/projectSection.js
@@ -1,24 +1,24 @@
-import React from 'react'
+import React from 'react';
 import Footer from '../../Footer/Footer.js';
 
 const projectSection = () => {
-  return (
-    <div className="frontPageProjectSectionMainContainer">
-      <div className="frontPageProjectSectionContainer">
-        <p id="frontPageProjectSectionTitle">『Our Projects』</p>
-        
-        <div className="frontPageProjectSectionProjectContainer">
+    return (
+        <div className="frontPageProjectSectionMainContainer">
+            <div className="frontPageProjectSectionContainer">
+                <h1 id="frontPageProjectSectionTitle">『Our Projects』</h1>
 
+                <div className="frontPageProjectSectionProjectContainer"></div>
+                <a className="frontPageProjectSectionReadMoreClick" href="/projects">
+                    <div className="frontPageProjectSectionReadMore">
+                        <button>
+                            <span>Read More!</span>
+                        </button>
+                    </div>
+                </a>
+            </div>
+            <Footer />
         </div>
-        <a className="frontPageProjectSectionReadMoreClick" href="/projects">
-          <div className="frontPageProjectSectionReadMore">
-            <p>Read More!</p>
-          </div>
-        </a>
-      </div>
-      <Footer/>
-    </div>
-  )
-}
+    );
+};
 
-export default projectSection
+export default projectSection;
