c0f7bb77fddc53a2d7578c335b0fd0b8e525cf55
Irwin <irwinngo@gmail.com>
2022-02-24 01:24:09 -0500

    Finished Project Section

--- src/components/pages/FrontPage/projectSlideInfo.js
+++ src/components/pages/FrontPage/projectSlideInfo.js
@@ -6,23 +6,28 @@ import Scanner from './images/scanner.jpg';
 
 export const projectSlideInfo = [
     {
-        text: "Short Description of Project",
+        id: 0,
+        text: "This treadmill allow users to move around in the VR while staying in one place",
         image: TreadMill,
     },
     {
-        text: "Short Description of Project",
+        id: 1,
+        text: "The Leap Motion Arcade is a multiplayer VR game console",
         image: Arcade,
     },
     {
-        text: "Short Description of Project",
+        id: 2,
+        text: "This project's goal is to integrate multiple Leap Motion sensors together",
         image: LeapHands,
     },
     {
-        text: "Short Description of Project",
+        id: 3,
+        text: "The Omni-Directional Shoes gives the experience of moving around in a VR space",
         image: Shoes,
     },
     {
-        text: "Short Description of Project",
+        id: 4,
+        text: "This 3D Room Scanner can easily scan a room to develop VR landscapes",
         image: Scanner,
     },
     
