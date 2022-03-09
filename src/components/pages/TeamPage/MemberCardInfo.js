96d387ff711a7e90b1878e19db733a3e60813f7f
Irwin <irwinngo@gmail.com>
2022-03-09 00:29:12 -0500

    Merge branch 'FrontPagePlusStyleBugFixes' of https://github.com/UTFO/UTOPH-Website

--- src/components/pages/TeamPage/MemberCardInfo.js
+++ src/components/pages/TeamPage/MemberCardInfo.js
@@ -3,6 +3,7 @@ import BenGoodchildImage from './images/Ben Goodchild.JPG';
 import GhamrSaeedImage from './images/Ghamr Saeed.jpg';
 import IrwinNgoImage from './images/Irwin Ngo.png';
 import ParthDuaImage from './images/Parth Dua.jpg';
+import LingBiImage from './images/Ling Bi.jpg';
 
 export const MemberCardInfo = [
     {
@@ -26,6 +27,12 @@ export const MemberCardInfo = [
     {
         name: 'Parth Dua',
         image: ParthDuaImage,
+        position: 'Position 1',
+        discipline: 'Discipline+Year 1'
+    },
+    {
+        name: 'Ling Bi',
+        image: LingBiImage,
         position: 'Position 1',
         discipline: 'Discipline+Year 1'
     }
